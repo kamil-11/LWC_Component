8021f810102cbebfe5d559c89980d01bd7c359e7
kamil-11 <kkamran336@gmail.com>
2023-10-04 22:16:12 +0530

    final component

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,14 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {
-
-    
+export default class HelloWorld extends LightningElement {    
     fullname ='salesforce India';
     title ='aura';
-
+                       
+    @track Address ={
+        Name : 'kamran',
+        age : '25',
+        City : 'Ranchi'
+    }
     changeHandler(event){
         //perform logic
         this.title = event.target.value;
      }
+     trackHandler(event){
+        this.Address.City = event.target.value;
+     }
+
+     userName = ['john', 'smith' , 'karen']; 
+     num = 10;
+     num2 = 17;
+     get firstUser()
+     {
+        return this.userName[0];
+     }
+     get Multiply(){
+     
+     return this.num * this.num2;
+     }
 
 }
+
+
+
+                                                        
+
+
+
+                                                        
+
+
